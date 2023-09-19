c79a032b17ca1ac3a2f50a2afaa1aeaafa18d2f9
Kevin Buffardi <kbuffardi@csuchico.edu>
2023-09-19 15:03:55 -0700

    Apply using to remove excessive std:: scope prefixes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
+using std::string;
+using std::cout;
+using std::getline;
+using std::endl;
+using std::cin;
+
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
-    std::string input;
+    string input;
 
     do{
-        std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        cout << "Enter a color in hex format (#RRGGBB):";
+        getline(cin, input);
 
         if( input.size() != RGB_HEX_LENGTH ){
-            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
-    std::cout << "Your hex color is: " << input << std::endl;
+    cout << "Your hex color is: " << input << endl;
 
     return 0;
 }
