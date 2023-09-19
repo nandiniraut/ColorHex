3404c8df956210d327eec74fc91adeb043910140
Kevin Buffardi <kbuffardi@csuchico.edu>
2023-09-18 19:34:26 -0700

    Remove namespace

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
-using namespace std;
 const int RGB_HEX_LENGTH = 7;
 
 int main(){
-    string input;
+    std::string input;
 
     do{
-        cout << "Enter a color in hex format (#RRGGBB):";
-        getline(cin, input);
+        std::cout << "Enter a color in hex format (#RRGGBB):";
+        std::getline(cin, input);
 
         if( input.size() != RGB_HEX_LENGTH ){
-            cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+            std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
-    cout << "Your hex color is: " << input << endl;
+    std::cout << "Your hex color is: " << input << std::endl;
 
     return 0;
 }
